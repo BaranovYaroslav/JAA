c8609709a3a546ceb76275984fdcb01d74266e40
YaroslavBaranov <ynbaranov9196@gmail.com>
2016-10-09 02:01:24 +0300

    Adding implementation of selection sort

---
+++ JAA/SearchingAlgorithms/SelectionSort/SelectionSort.java
@@ -0,0 +1,33 @@
+package SearchingAlgorithms;
+
+import java.util.Comparator;
+
+public class SelectionSort<T extends Comparable> {
+
+    public T[] sort(T[] array) {
+        return sort(array, new Comparator<T>() {
+            @Override
+            public int compare(T el1, T el2) {
+                return el1.compareTo(el2);
+            }
+        });
+    }
+
+    public T[] sort(T[] array, Comparator<T> comparator) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if(comparator.compare(array[min], array[j]) == 1){
+                    min = j;
+                }
+            }
+            if(min != i) {
+                T temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+        }
+        return  array;
+    }
+
+}
