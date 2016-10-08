b0c65c13092c13c247abd3deea3097479b94f0a7
YaroslavBaranov <ynbaranov9196@gmail.com>
2016-10-09 01:46:25 +0300

    Adding implementation of insertion sort

---
+++ JAA/SearchingAlgorithms/InsertionSort/InsertionSort.java
@@ -0,0 +1,28 @@
+package SearchingAlgorithms;
+
+import java.util.Comparator;
+
+public class InsertionSort<T extends Comparable> {
+
+    public T[] sort(T[] array) {
+        return sort(array, new Comparator<T>() {
+            @Override
+            public int compare(T el1, T el2) {
+                return el1.compareTo(el2);
+            }
+        });
+    }
+
+    public T[] sort(T[] array, Comparator<T> comparator) {
+        for (int j = 1; j < array.length; j++) {
+            T key = array[j];
+            int i = j - 1;
+            while(i >= 0 && comparator.compare(array[i], key) == 1){
+                array[i+1] = array[i];
+                i--;
+            }
+            array[i+1] = key;
+        }
+        return array;
+    }
+}
