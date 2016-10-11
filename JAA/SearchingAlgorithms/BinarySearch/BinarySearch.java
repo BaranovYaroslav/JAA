b2e284765b3ade588ddcaad7f557d3f172584bdd
YaroslavBaranov <ynbaranov9196@gmail.com>
2016-10-12 00:48:17 +0300

    Adding binary search implementation

---
+++ JAA/SearchingAlgorithms/BinarySearch/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch{
+
+    public static int find(int[] array, int element){
+
+        int index = -1;
+
+        int low = 0;
+        int high = array.length;
+        int middle = 0;
+
+        while (low <= high) {
+            middle = (low + high) / 2;
+            System.out.println(5/2);
+            if(array[middle] == element){
+                index = middle;
+                break;
+            }
+            else {
+                if(array[middle] < element){
+                    low = middle + 1;
+                }
+                else{
+                    high = middle - 1;
+                }
+            }
+        }
+        return index;
+    }
+
+}
